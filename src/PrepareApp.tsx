55ef5eda12a4cf81e52a3306b044dbdc4b48ecd2
Catholic Dev <catholicdesign.dev@gmail.com>
2023-09-20 12:17:51 +0700

    feat: update code to check token available

--- src/PrepareApp.tsx
+++ src/PrepareApp.tsx
@@ -4,6 +4,7 @@ import { useDispatch, setToken } from '@providers'
 import { Loading } from '@components'
 import { useHistory } from 'react-router-dom'
 import { routes } from '@routes'
+import { useLazyGetCheckTokenQuery } from '@api'
 
 interface Props {
   children: JSX.Element
@@ -14,19 +15,31 @@ export const PrepareApp = ({ children }: Props) => {
   const dispatch = useDispatch()
   const { push, replace } = useHistory()
 
+  const [getCheckToken, { isLoading, error }] = useLazyGetCheckTokenQuery()
+
+  useEffect(() => {
+    getCheckToken()
+  }, [])
+
   useEffect(() => {
     const loadToken = async () => {
-      const token = await Storage.get({ key: 'hojoToken' })
-
-      if (!!token.value) {
-        dispatch(setToken(token.value))
-        push(routes.Home)
-      } else {
+      try {
+        if (!isLoading && !error) {
+          const token = await Storage.get({ key: 'hojoToken' })
+          dispatch(setToken(token.value ?? ''))
+          push(routes.Home)
+        } else {
+          await Storage.remove({ key: 'hojoToken' })
+          replace(routes.OnBoarding)
+        }
+      } catch (err) {
+        console.log(err)
+        await Storage.remove({ key: 'hojoToken' })
         replace(routes.OnBoarding)
       }
     }
     loadToken().then(() => setLoaded(true))
-  }, [dispatch, push, replace])
+  }, [dispatch, push, replace, isLoading, error])
 
   return <Loading loading={!loaded}>{children}</Loading>
 }
